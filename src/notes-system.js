643ba95ca91a45e51d089e62a323fd6c93c2c69b
Shaneel Kumar <shaneel_kumar@live.com>
2022-05-10 22:47:19 +1200

    Refactoring notes system.

--- src/notes-system.js
+++ src/notes-system.js
@@ -13,26 +13,30 @@ const notesManager = (function(){
     addToProjectList(project2)
 
     const note1 = createNewNote('Leaving New Zealand')
-    note1.addNewItem('Book tickets')
-    note1.addNewItem('Pack bags')
-    note1.addNewItem('Book public transport')
+    note1.addNewNoteItem('Book tickets')
+    note1.addNewNoteItem('Pack bags')
+    note1.addNewNoteItem('Book public transport')
 
     const note2 = createNewNote('London Itinerary')
-    note2.addNewItem('See London eye')
-    note2.addNewItem('Visit Hambden Markets')
-    note2.addNewItem('Grab a drink at Soho')
+    note2.addNewNoteItem('See London eye')
+    note2.addNewNoteItem('Visit Hambden Markets')
+    note2.addNewNoteItem('Grab a drink at Soho')
     
     project1.addNewNote(note1)
     project1.addNewNote(note2)
 
-    function createNewNote(noteTitle) {
-        const noteList = []
+    function assignNewNoteToProject(note, project) {
+        project.addNewNote(note)
+    }
+
+    function createNewNote(title) {
+        const items = []
+        function addNewNoteItem(note) { items.push(note) }
+
         return {
-            title: noteTitle,
-            items: noteList,
-            addNewItem: function(note) {
-                noteList.push(note)
-            },
+            title,
+            items,
+            addNewNoteItem
         }
     }
 
@@ -44,32 +48,32 @@ const notesManager = (function(){
             addNewNote: function(note) {
                 notes.push(note)
             },
-            logAllNotes: function() {
-                notes.forEach(note => {
-                    console.table(note)
-                })
-            }
         }
     }
-    function removeFromProjects(removeNote) {
+
+    function addToProjectList(project) { projects.push(project) }
+
+    function removeFromProjectList(project) {
+        const index = projects.indexOf(project)
+        projects.splice(index, 1)
+    }
+
+    function removeNoteFromProject(noteToRemove) {
         projects.forEach(project => { // Object
             project.notes.forEach(note => { // Object
                 const currentIndex = project.notes.indexOf(note)
-                if (note == removeNote) {
+                if (note == noteToRemove) {
                     project.notes.splice(currentIndex, 1)
                 }
             })
         })
     }
 
-    removeFromProjects('London Itinerary')
-    function addToProjectList(project) {projects.push(project)}
-
     return {
         projects,
         createNewNote,
         addToProjectList,
-        removeFromProjects,
+        removeFromProjects: removeNoteFromProject,
     }
 })()
 
