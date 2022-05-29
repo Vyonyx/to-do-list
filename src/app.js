54dbdbcb1510b15d1795ef70ce671b7dc87b0358
Shaneel Kumar <shaneel_kumar@live.com>
2022-05-29 13:53:41 +1200

    Added project data structure and DOM rendering.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,75 @@
 import './scss/style.scss'
-import notesManager from './notes-manager'
+
+const projectsList = document.querySelector('[data-projects]')
+const newProjectForm = document.querySelector('[data-new-project-form]')
+const newProjectInput = document.querySelector('[data-new-project-input]')
+
+let LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = 'todo.projectID'
+let LOCAL_STORAGE_PROJECTS_KEY = 'todo.projects'
+
+// localStorage.clear()
+// let projects =  []
+let projects =  JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECTS_KEY)) || []
+let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY)
+
+// Add new project to projects list.
+newProjectForm.addEventListener('submit', e => {
+    e.preventDefault()
+    addProject(newProjectInput.value)
+    newProjectInput.value = ''
+})
+
+// Change the stored selected project ID.
+projectsList.addEventListener('click', e => {
+    if (!e.target.tagName.toLowerCase() === 'li') return
+    selectedProjectId = e.target.id
+    saveAndRender()
+})
+
+function addProject(title) {
+    const newProject = {
+        title,
+        id: Date.now().toString(),
+        tasks: []
+    }
+    projects.push(newProject)
+    saveAndRender()
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+// Renders any updates to the screen.
+function render() {
+    renderProjectList()
+}
+
+// Save things to local storage.
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY, selectedProjectId)
+    localStorage.setItem(LOCAL_STORAGE_PROJECTS_KEY, JSON.stringify(projects))
+}
+
+// Take the projects in the array of objects variable and create an updated list.
+function renderProjectList() {
+    clearElements(projectsList)
+    for (let project of projects) {
+        const newProject = document.createElement('li')
+        newProject.innerText = project.title
+        newProject.classList.add('project-item')
+        newProject.id = project.id
+        if (newProject.id === selectedProjectId) { newProject.classList.add('active-project') }
+        projectsList.appendChild(newProject)
+    }
+}
+
+// Clear all the elements in a DOM container.
+function clearElements(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
+
+render()
